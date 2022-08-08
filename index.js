653ab2c8ff931bb1cf319a77e72dd75b3a52efb0
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-08-08 15:40:18 +0200

    building Reoutes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 5000
 const authorsRoutes = require('./routes/authors')
 
+
 app.use('/authors', authorsRoutes)
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
--- routes/authors.js
+++ routes/authors.js
@@ -28,6 +28,102 @@ app.get('/authors', (req, res) => {
       books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
   },
   ]
+
+  res.json(authors)
+})
+
+app.get('/', (req, res) => {
+  res.json("Authors API")
+})
+
+app.get('/authors/:id', (req, res) => {
+  const authors = [
+    {
+      id: 1,
+      name: "Lawrence Nowell",
+      nationality: "UK",
+      books: ["Beowulf"]
+  },
+  {
+      id: 2,
+      name: "William Shakespeare",
+      nationality: "UK",
+      books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+  },
+  {
+      id: 3,
+      name: "Charles Dickens",
+      nationality: "US",
+      books: ["Oliver Twist", "A Christmas Carol"]
+  },
+  {
+      id: 4,
+      name: "Oscar Wilde",
+      nationality: "UK",
+      books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+  },
+  ]
+  res.json(authors.name, authors.nationality)
+})
+
+app.get('/authors/:id/books', (req, res) => {
+  const authors = [
+    {
+      id: 1,
+      name: "Lawrence Nowell",
+      nationality: "UK",
+      books: ["Beowulf"]
+  },
+  {
+      id: 2,
+      name: "William Shakespeare",
+      nationality: "UK",
+      books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+  },
+  {
+      id: 3,
+      name: "Charles Dickens",
+      nationality: "US",
+      books: ["Oliver Twist", "A Christmas Carol"]
+  },
+  {
+      id: 4,
+      name: "Oscar Wilde",
+      nationality: "UK",
+      books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+  },
+  ]
+  res.json(authors.books)
+})
+
+app.get('json//authors/:id/books', (req, res) => {
+  const authors = [
+    {
+      id: 1,
+      name: "Lawrence Nowell",
+      nationality: "UK",
+      books: ["Beowulf"]
+  },
+  {
+      id: 2,
+      name: "William Shakespeare",
+      nationality: "UK",
+      books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+  },
+  {
+      id: 3,
+      name: "Charles Dickens",
+      nationality: "US",
+      books: ["Oliver Twist", "A Christmas Carol"]
+  },
+  {
+      id: 4,
+      name: "Oscar Wilde",
+      nationality: "UK",
+      books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+  },
+  ]
+  res.json(authors.name, authors.nationality)
 })
 
 module.exports = app
